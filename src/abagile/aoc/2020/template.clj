c0d70cdf91ab9bff4c2a6b1294196e4d70895914
Szeto Bo <szetobo@gmail.com>
2020-12-19 17:37:09 +0800

    Add template for easier kick-starting

---
+++ src/abagile/aoc/2020/template.clj
@@ -0,0 +1,18 @@
+(ns abagile.aoc.2020.template
+  (:gen-class)
+  (:require
+    [abagile.aoc.util :as util]))
+
+(def input (->> (util/read-input-split-lines "2020/day.txt")))
+
+(def sample [])
+
+(comment)
+
+(defn part1 [])
+
+(defn part2 [])
+
+(defn -main [& _]
+  (println "part 1:" (part1))
+  (println "part 2:" (part2)))
