7efa2aa88632203f791f3c0ed23913d2046a16ed
Szeto Bo <szetobo@gmail.com>
2020-12-22 11:20:55 +0800

    Update template to add instaparser parser by default

--- src/abagile/aoc/2020/template.clj
+++ src/abagile/aoc/2020/template.clj
@@ -1,11 +1,21 @@
 (ns abagile.aoc.2020.template
   (:gen-class)
   (:require
-    [abagile.aoc.util :as util]))
+    [abagile.aoc.util :as util]
+    [clojure.test :refer [deftest run-tests is]]
+    [instaparse.core :as insta]))
 
 ; (def input (->> (util/read-input-split-lines "2020/day.txt")))
 
 (def sample [])
+
+(def rule-parser (insta/parser "S = 'testing'"))
+
+(def transform-opts {:I identity})
+
+(defn parse [data]
+  (->> (map rule-parser data)
+       (map (partial insta/transform transform-opts))))
 
 (comment)
 
@@ -16,3 +26,11 @@
 (defn -main [& _]
   (println "part 1:" (part1))
   (println "part 2:" (part2)))
+
+(deftest test-sample
+  (is (= 1 1)))
+
+(deftest test-input
+  (is (= 1 1)))
+
+(run-tests)
