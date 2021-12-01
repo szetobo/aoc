3d274c8ad99fcee65d86a546048aedb038c7c59f
Szeto Bo <szetobo@gmail.com>
2021-12-01 14:07:02 +0800

    Add 2021.01 solution

---
+++ src/abagile/aoc/2021/day01.clj
@@ -0,0 +1,36 @@
+(ns abagile.aoc.2021.day01
+  (:gen-class)
+  (:require
+    [abagile.aoc.util :as util]))
+
+(def input (->> (util/read-input-split-lines "2021/day01.txt")
+                (map util/parse-int)))
+
+(comment
+  (count input)
+  (take 10 input)
+  (partition 2 1 input)
+  (partition 3 1 input))
+
+(defn count-if
+  [pred coll]
+  (->> (partition 2 1 coll)
+       (filter (fn [[n1 n2]] (pred n2 n1)))
+       count))
+
+(defn part1
+  []
+  (time (count-if > input)))
+
+(defn part2
+  []
+  (time (->> (partition 3 1 input)
+             (map #(reduce + %))
+             (count-if >))))
+
+(defn -main [& _]
+  (println "part 1:" (part1))
+  (println "part 2:" (part2)))
+
+(comment
+  (-main))
