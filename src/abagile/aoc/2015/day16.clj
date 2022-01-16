f40ebed1e38f0648cba27da15dedd307d2d0e31e
Szeto Bo <szetobo@gmail.com>
2022-01-16 16:48:17 +0800

    Update 2015.16 solution

--- src/abagile/aoc/2015/day16.clj
+++ src/abagile/aoc/2015/day16.clj
@@ -3,57 +3,48 @@
   (:require
     [abagile.aoc.util :as util]
     [clojure.set :as set]
-    [clojure.test :refer [deftest is]]
-    [instaparse.core :as insta]))
+    [clojure.test :refer [deftest is]]))
 
-(def input (->> (util/read-input-split-lines "2015/day16.txt")))
+(def input (util/read-input-split "2015/day16.txt" #"\n"))
 
-(def target {:children 3 :cats 7 :samoyeds 2 :pomeranians 3 :akitas 0
-             :vizslas 0 :goldfish 5 :trees 3 :cars 2 :perfumes 1})
+(def target {:children 3 :cats     7 :samoyeds 2 :pomeranians 3 :akitas   0
+             :vizslas  0 :goldfish 5 :trees    3 :cars        2 :perfumes 1})
 
-(def rule-parser (insta/parser "S = <'Sue '> SUE <':'> PROPS
-                                SUE = #'\\d+'
-                                PROPS = PROP | PROP (<','> PROP)+
-                                <space> = <#'\\s*'>
-                                PROP = PT QTY
-                                PT = space #'\\w+' <':'> space
-                                QTY = #'[0-9]+'"))
-
-(def transform-opts {:S vector
-                     :SUE util/parse-int
-                     :PROPS merge
-                     :PROP hash-map
-                     :PT keyword
-                     :QTY util/parse-int})
-
-(defn parse [data]
-  (->> (map rule-parser data)
-       (map (partial insta/transform transform-opts))))
+(defn parse
+  [data]
+  (into {} (map vector
+             (map #(->> (re-find #"Sue (\d+):" %) second read-string) data)
+             (map #(->> (re-seq #"(\w+): (\d+)" %)
+                        (map (fn [[_ k v]] [(keyword k) (read-string v)]))
+                        (into {}))
+               data))))
 
 (defn part1 []
-  (time (let [ts (set target)]
-         (->> (map (fn [[k v]] (when (set/subset? (set v) ts) k)) (parse input))
-              (filter some?)
-              first))))
+  (time
+    (let [ts (set target)]
+     (->> (parse input)
+          (filter (fn [[_ v]] (set/subset? (set v) ts)))
+          first))))
 
 (defn part2 []
-  (time (->> (map
-               (fn [[k v]]
-                 (when (reduce (fn [res [k' v']]
-                                 (let [tv (target k')]
-                                   (and res (case k'
-                                              :cats :trees (> v' tv)
-                                              :pomeranians :goldfish (< v' tv)
-                                              (= v' tv)))))
-                               true v)
-                   k))
-               (parse input))
-            (filter some?)
-            first)))
+  (time
+    (->> (parse input)
+         (filter (fn [[_ m]]
+                   (every? (fn [[k v]]
+                             (let [tv (target k)]
+                               (case k
+                                 :cats :trees           (> v tv)
+                                 :pomeranians :goldfish (< v tv)
+                                 (= v tv))))
+                     m)))
+         first)))
 
 (defn -main [& _]
   (println "part 1:" (part1))
   (println "part 2:" (part2)))
 
-(deftest test-input
-  (is (= (first (parse input)) {1 {:cars 9, :goldfish 0, :akitas 3}})))
+(comment
+  (-main))
+
+(deftest example
+  (is (= 0 0)))
