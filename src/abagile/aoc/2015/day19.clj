41185fd468888144bc8037921d96af6fa73c1479
Szeto Bo <szetobo@gmail.com>
2022-01-18 10:37:10 +0800

    Update 2015.19 solution

--- src/abagile/aoc/2015/day19.clj
+++ src/abagile/aoc/2015/day19.clj
@@ -5,44 +5,44 @@
     [clojure.string :as cs]
     [clojure.test :refer [deftest is]]))
 
-(def input (->> (util/read-input-split "2015/day19.txt" #"\n\n")))
+(def sample (->> (util/read-input-split "2015/day19.sample.txt" #"\n\n")))
+(def input  (->> (util/read-input-split "2015/day19.txt" #"\n\n")))
 
-(def sample ["H => HO
-              H => OH
-              O => HH"
-             "HOH"])
+(defn parse
+  [[rules input]]
+  [(->> rules (re-seq #"(\w+) => (\w+)") (map rest))
+   (->> input (re-find #"\w+"))])
 
-(defn parse [s] (->> (re-seq #"(\w+) => (\w+)" s)
-                     (map next)))
-
-(defn replacement [data]
-  (let [[rr target] data]
-    (->> (for [[fr to] (parse rr)
-               :let [fr-size (count fr)]]
-           (loop [start 0
-                  rec   []]
-             (if-let [i (cs/index-of target fr start)]
-               (recur (+ i fr-size) (conj rec (str (subs target 0 i) to (subs target (+ i fr-size)))))
-               rec)))
-         (reduce concat)
-         set)))
+(defn replacement
+  [[rules input]]
+  (->> (for [[fr to] rules :let [fr-size (count fr)]]
+         (loop [start 0 rec []]
+           (if-let [i (cs/index-of input fr start)]
+             (recur (+ i fr-size) (conj rec (str (subs input 0 i) to (subs input (+ i fr-size)))))
+             rec)))
+       (reduce concat) set))
 
 (defn part1 []
-  (time (count (replacement input))))
+  (time
+    (->> (parse input) replacement count)))
 
 (defn part2 []
-  (time (let [molecule (second input)]
-         (- (count (re-seq #"[A-Z]" molecule))
-            (+
-              (count (re-seq #"Rn" molecule))
-              (count (re-seq #"Ar" molecule))
-              (* 2 (count (re-seq #"Y" molecule)))
-              1)))))
+  (time
+    (let [molecule (second (parse input))]
+     (- (count (re-seq #"[A-Z]" molecule))
+        (+
+          (count (re-seq #"Rn" molecule))
+          (count (re-seq #"Ar" molecule))
+          (* 2 (count (re-seq #"Y" molecule)))
+          1)))))
 
 (defn -main [& _]
   (println "part 1:" (part1))
   (println "part 2:" (part2)))
 
+(comment
+  (-main))
+
 (deftest test-sample
-  (is (= (parse (first sample)) [["H" "HO"] ["H" "OH"], ["O" "HH"]]))
-  (is (= (replacement sample) #{"HOOH" "HOHO" "OHOH" "HHHH"})))
+  (is (= (parse sample) [[["H" "HO"] ["H" "OH"], ["O" "HH"]] "HOH"]))
+  (is (= (replacement (parse sample)) #{"HOOH" "HOHO" "OHOH" "HHHH"})))
