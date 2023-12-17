0140854df15854730d2907cd800edf169bb543c6
Szeto Bo <szetobo@gmail.com>
2023-12-17 15:24:02 +0800

    Update 2023 template

--- src/abagile/aoc/2023/template.clj
+++ src/abagile/aoc/2023/template.clj
@@ -1,17 +1,29 @@
 (ns abagile.aoc.2023.dayXX
   (:require
-   [abagile.aoc.util :as util]))
+   [abagile.aoc.util :as util]
+   [clojure.set :as set]
+   [clojure.string :as cs]
+   [clojure.test :refer [deftest is]]))
 
-(def input (->> (util/read-input-split "2023/dayXX.txt" #"\n")))
+;; (def input (->> (util/read-input "2023/dayXX.txt"))
+;; (def input (->> (util/read-input-split "2023/dayXX.txt" #"\n")))
+
+(deftest test1
+  (is (= 1 1)))
 
 (defn part1
   []
-  (time (->> input)))
+  (time
+   1))
 
+
+(deftest test2
+  (is (= 1 1)))
 
 (defn part2
   []
-  (time (->> input)))
+  (time
+   1))
 
 (defn -main [& _]
   (println "part 1:" (part1))
