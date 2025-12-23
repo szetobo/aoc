530d97aaace20378d2d63346f630b3fba936e4ef
Szeto Bo <szetobo@gmail.com>
2025-12-23 08:06:55 +0800

    Add 2025.01 ts solution

---
+++ ts/2025/day01.ts
@@ -0,0 +1,18 @@
+let part1 = 0, part2 = 0
+let pos = 50
+for await (const line of console) {
+	if (line == "") continue
+	const tokens = line.split(/(L|R|\d+)/).filter(Boolean);
+	const dir = tokens[0] == "L" ? -1 : 1
+	for (let i = 0; i < Number(tokens[1]); i++) {
+		pos = (pos + dir) % 100
+		if (pos == 0) {
+			part2++
+		}
+	}
+	if (pos == 0) {
+		part1++
+	}
+}
+console.log("The result for part 1: %d", part1)
+console.log("The result for part 2: %d", part2)
