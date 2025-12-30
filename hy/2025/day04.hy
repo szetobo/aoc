6046f155e391bcd8bfa9831fb4ecfc30f00ef024
Szeto Bo <szetobo@gmail.com>
2025-12-30 21:11:02 +0800

    Add 2025.04 hy solution

---
+++ hy/2025/day04.hy
@@ -0,0 +1,40 @@
+(import sys)
+(import itertools [count product])
+(import hyrule *)
+(require hyrule *)
+
+(setv p1 0
+      p2 0)
+
+(setv D (.read sys.stdin)
+      lines (lfor line (.split D "\n") :if line (list line)))
+
+(defn adjacent8 [c limit]
+	(setv [row col] c
+	      offsets [[-1 -1] [-1 0] [-1 1]
+		       [0 -1]         [0 1]
+		       [1 -1]  [1 0]  [1 1]])
+	(lfor [dr dc] offsets
+		:setv nr (+ row dr)
+		:setv nc (+ col dc)
+		:if (and (<= 0 nr (dec limit))
+			 (<= 0 nc (dec limit)))
+		#(nr nc)))
+
+(for [i (count 0)]
+	(setv curr p2)
+	(for [[row col] (product (range (len lines)) (range (len lines)))]
+		(when (= "@" (get lines row col))
+			(setv cnt (len (lfor [r c] (adjacent8 [row col] (len lines))
+					       :if (= "@" (get lines r c))
+					       #(r c))))
+			(when (< cnt 4)
+				(if (= i 0)
+					(setv p1 (inc p1))
+					(do (setv (get lines row col) "x")
+					    (setv p2 (inc p2)))))))
+	(when (and (> i 0) (= curr p2))
+		(break)))
+
+(print f"The result for part 1: {p1}")
+(print f"The result for part 2: {p2}")
--- py/2025/day04.py
+++ py/2025/day04.py
@@ -1,56 +1,43 @@
 import sys
-import itertools
-from typing import NamedTuple
+from itertools import count, product
 
 
-class Pt(NamedTuple):
-    row: int
-    col: int
-
-
-def adjacent8(c: Pt, limit: int) -> list[Pt]:
-    offset: list[Pt] = [
-        Pt(-1, -1),
-        Pt(-1, 0),
-        Pt(-1, 1),
-        Pt(0, -1),
-        Pt(0, 1),
-        Pt(1, -1),
-        Pt(1, 0),
-        Pt(1, 1),
+def adjacent8(c: tuple[int, int], limit: int) -> list[tuple[int, int]]:
+    row, col = c
+    offset: tuple[tuple[int, int], ...] = (
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    )
+    return [
+        (row, col)
+        for (row, col) in [(row + dr, col + dc) for (dr, dc) in offset]
+        if 0 <= row < limit and 0 <= col < limit
     ]
-    adj: list[Pt] = []
-    for pt in [Pt(c.row + pt.row, c.col + pt.col) for pt in offset]:
-        if all(v >= 0 and v < limit for v in pt):
-            adj.append(pt)
-    return adj
 
 
-part1, part2 = 0, 0
-inputs: list[list[str]] = []
-while line := sys.stdin.readline().strip():
-    inputs.append([ch for ch in line])
+p1, p2 = 0, 0
+D = sys.stdin.read()
+lines: list[list[str]] = [list(line) for line in D.split("\n") if line]
+n = len(lines)
 
-for i in itertools.count(0):
-    done = part2
-    for row, line in enumerate(inputs):
-        for col, ch in enumerate(line):
-            if ch == "@":
-                cnt = len(
-                    [
-                        pt
-                        for pt in adjacent8(Pt(row, col), len(inputs))
-                        if inputs[pt.row][pt.col] == "@"
-                    ]
-                )
-                if cnt < 4:
-                    if i == 0:
-                        part1 += 1
-                    else:
-                        line[col] = "x"
-                        part2 += 1
-    if i > 0 and done == part2:
+for i in count(0):
+    done = p2
+    for row, col in product(range(n), range(n)):
+        if lines[row][col] == "@":
+            if sum(1 for (r, c) in adjacent8((row, col), n) if lines[r][c] == "@") < 4:
+                if i == 0:
+                    p1 += 1
+                else:
+                    lines[row][col] = "x"
+                    p2 += 1
+    if i > 0 and done == p2:
         break
 
-print(f"The result for part 1: {part1}")
-print(f"The result for part 2: {part2}")
+print(f"The result for part 1: {p1}")
+print(f"The result for part 2: {p2}")
